aa757e5f15c1379da6a91c87af62f24696ef02b6
Keisuke KOIKE <keisuke.koike@dena.jp>
2019-05-21 13:09:23 +0900

    Add Mole popping logic and whack handler

--- src/App.vue
+++ src/App.vue
@@ -51,10 +51,12 @@ export default {
       this.resetState();
       this.gameActive = true;
       this.startTimer();
+      this.startMoles();
     },
     endGame: function() {
       this.gameActive = false;
       this.stopTimer();
+      this.stopMoles();
     },
     startTimer: function() {
       this.timerId = setInterval( () => {
@@ -70,8 +72,33 @@ export default {
     stopTimer: function() {
       clearInterval(this.timerId);
     },
+    startMoles: function() {
+      this.moleInterval = setInterval(this.activateRandomMole.bind(this), 500);
+    },
+    stopMoles: function() {
+      clearInterval(this.moleInterval);
+    },
+    activateRandomMole: function() {
+      const randomMoleIndex = Math.floor(Math.random() * this.moles.lengeth);
+      if (!this.moles[randomMoleIndex]) {
+        this.activateMole(randomMoleIndex);
+      }
+    },
+    toggleMole: function() {
+      const moles = this.moles.slice();
+      moles[moleId] = shouldShow;
+      this.moles = moles;
+    },
+    activateMole: function(moleId) {
+      this.toggleMole(moleId, true);
+      setTimeout(() => this.deactivateMole(moleId, 1500));
+    },
+    deactivateMole: function() {
+      this.toggleMole(moleId, false);
+    },
     handleMoleWhack: function(moleId) {
-      console.log('moleId = ', moleId);
+      this.score = this.score + 1;
+      this.deactivateMole(moleId);
     },
   },
 };
